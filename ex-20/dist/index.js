877d862fa64ff48173d270cd79ae077a23cd4c12
Way2logic <128026796+waytologic@users.noreply.github.com>
2024-01-30 19:59:16 +0530

    ex-20

---
+++ ex-20/dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+class MenuItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    get details() {
+        return `${this.title} - £${this.price}`;
+    }
+}
+class Pizza extends MenuItem {
+    constructor(title, price) {
+        super(title, price);
+        this.base = 'classic';
+        this.toppings = [];
+    }
+    addTopping(topping) {
+        this.toppings.push(topping);
+    }
+    removeTopping(topping) {
+        this.toppings = this.toppings.filter((t) => t !== topping);
+    }
+    selectBase(b) {
+        this.base = b;
+    }
+}
+const pizza = new Pizza('mario special', 15);
+function printMenuItem(item) {
+    console.log(item.details);
+}
+printMenuItem(pizza);
---
+++ ex-20/src/index.ts
@@ -0,0 +1,36 @@
+type Base = 'classic' | 'thick' | 'thin' | 'garlic'
+
+class MenuItem {
+	constructor(private title: string, private price: number) {}
+
+	get details(): string {
+		return `${this.title} - £${this.price}`
+	}
+}
+
+class Pizza extends MenuItem {
+	constructor(title: string, price: number) {
+		super(title, price)
+	}
+
+	private base: Base = 'classic'
+	private toppings: string[] = []
+
+	addTopping(topping: string): void {
+		this.toppings.push(topping)
+	}
+	removeTopping(topping: string): void {
+		this.toppings = this.toppings.filter((t) => t !== topping)
+	}
+	selectBase(b: Base): void {
+		this.base = b
+	}
+}
+
+const pizza = new Pizza('mario special', 15)
+
+function printMenuItem(item: MenuItem): void {
+	console.log(item.details)
+}
+
+printMenuItem(pizza)
